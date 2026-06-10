8ba17341243c00a1c58ae4e2080bc48e6320c6e5
Xchuvcf <gameskurdish655@gmail.com>
2026-06-10 13:46:00 +0300

    Update Firebase configuration with new credentials

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBJGIsMhKlucjvP9XY5MThhcPyZfQRAc0Y",
+    authDomain: "x-project-94a25.firebaseapp.com",
+    databaseURL: "https://x-project-94a25-default-rtdb.firebaseio.com",
+    projectId: "x-project-94a25",
+    storageBucket: "x-project-94a25.firebasestorage.app",
+    messagingSenderId: "773919021884",
+    appId: "1:773919021884:web:35c525cfa8e71e91835c39"
 };
 
 firebase.initializeApp(firebaseConfig);
